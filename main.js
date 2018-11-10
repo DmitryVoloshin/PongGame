ded881331d506e92d280af12aebc18e4f69e6f39
Dr.Voloshin <eralashpvp@gmail.com>
2018-11-10 20:06:28 +0200

    adding rows

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ var ballSpeedY = 7;
 
 const BRICK_W = 100;
 const BRICK_H = 50;
-const BRICK_COUNT = 4;
+const BRICK_COUNT = 8;
+const BRICK_ROWS = 2;
+const BRICK_GAP = 2;
 
-var brickGrid = [ true,false,true,true];
+var brickGrid = new Array(BRICK_COUNT);
 
 
 const PADDLE_WIDTH = 100;
@@ -32,7 +34,11 @@ function updateMousePos(evt){
     paddleX = mouseX - PADDLE_WIDTH/2;
 
 }
-
+function brickReset(){
+    for(var i=0;i<BRICK_COUNT;i++){
+        brickGrid[i] = true;
+    }
+}
 
 window.onload = function(){
     canvas = this.document.getElementById('gameCanvas');
@@ -42,7 +48,9 @@ window.onload = function(){
    this.setInterval(updateAll, 1000/framesPerSecond);
 
 
-   canvas.addEventListener('mousemove',updateMousePos)
+   canvas.addEventListener('mousemove',updateMousePos);
+
+   brickReset();
 }
   
 
@@ -101,7 +109,16 @@ if( ballY > paddleTopEdgeY &&
 function drawBricks(){
     for(var i=0;i<BRICK_COUNT;i++){
         if(brickGrid[i]){
-            colorRect(BRICK_W*i,0, BRICK_W-2,BRICK_H, 'red');
+            colorRect(BRICK_W*i,0, 
+                BRICK_W-BRICK_GAP,
+                BRICK_H-BRICK_GAP, 'red');
+        }
+    }
+
+    for(var i=0;i<BRICK_COUNT;i++){
+        if(brickGrid[i]){
+            colorRect(BRICK_W*i,BRICK_H, BRICK_W-BRICK_GAP
+                ,BRICK_H-BRICK_GAP, 'red');
         }
     }
 }
@@ -115,7 +132,9 @@ function drawAll(){
 
     drawBricks();
 
-    colorText(mouseX+","+mouseY,mouseX,mouseY,'yellow');
+    var mouseBrickCol = mouseX / BRICK_W;
+    var mouseBrickRow = mouseY / BRICK_H;
+    colorText(mouseBrickCol+","+mouseBrickRow,mouseX,mouseY,'yellow');
 
  }
 
